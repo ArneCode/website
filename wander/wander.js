8f48f48f9e92b2743235fd83d6da03bf0e858aeb
ArneLaptop <arne.deborman@web.de>
2026-03-20 21:43:54 +0100

    Update wander console link to new URL

--- wander/wander.js
+++ wander/wander.js
@@ -1,7 +1,7 @@
 window.wander = {
     // Other Wander consoles that visitors can reach from my console.
     consoles: [
-        'https://wander-console.vercel.app/',
+        'https://susam.net/wander/',
     ],
     // My favourite websites and pages I recommend to the Wander community.
     pages: [
