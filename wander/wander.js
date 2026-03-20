d967f5fa6be41f3b23ec23c5c2f3ed45e3947ecd
ArneLaptop <arne.deborman@web.de>
2026-03-20 21:36:16 +0100

    added wander functionality

---
+++ wander/wander.js
@@ -0,0 +1,10 @@
+window.wander = {
+    // Other Wander consoles that visitors can reach from my console.
+    consoles: [
+        'https://wander-console.vercel.app/',
+    ],
+    // My favourite websites and pages I recommend to the Wander community.
+    pages: [
+        'https://blog.arnedebo.com/posts/test-post/'
+    ],
+}
