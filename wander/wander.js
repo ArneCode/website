2820d96e096bbd1afaf34f2555c8b37e392db149
ArneLaptop <arne.deborman@web.de>
2026-03-21 11:30:45 +0100

    pulled new wander console

--- wander/wander.js
+++ wander/wander.js
@@ -2,10 +2,10 @@ window.wander = {
     // Other Wander consoles that visitors can reach from my console.
     consoles: [
         'https://susam.net/wander/',
-        'https://arnedebo.com/wander/malicious/'
     ],
     // My favourite websites and pages I recommend to the Wander community.
     pages: [
         'https://blog.arnedebo.com/posts/test-post/',
+        'https://arnedebo.com/',
     ],
 }
